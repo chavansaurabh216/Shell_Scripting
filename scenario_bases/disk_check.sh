599ae2d48e44670797ffe5b6a249e82d088d0aa0
chavansaurabh216 <chavansaurabh216@gmail.com>
2025-02-17 14:49:29 +0530

    testing the scenario

---
+++ scenario_bases/disk_check.sh
@@ -0,0 +1,4 @@
+# Scenario 3: Disk Space Monitor Script Using Conditionals
+
+# Objective: Create a script called disk_check.sh that checks the disk usage of a specified mount point and alerts you if the usage exceeds a certain threshold. 
+
